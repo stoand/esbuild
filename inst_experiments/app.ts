6971e6f2d8baf71e63e9e8e5dce99277d8872539
Andreas Stocker <shadowstep7@gmail.com>
2023-03-01 21:03:15 +0100

    work on #184: remove inst_experiments out file

--- inst_experiments/app.ts
+++ inst_experiments/app.ts
@@ -3,9 +3,14 @@ const asdf = 1234;
 function asdf2() {
     console.log('asdf');
     
+    
     let b = 3;
     let c = 4;
     let a = b + 1 + c;
+    
+    if (b == 3 || c == 4) {
+        console.log('done');
+    }
 }
 
 asdf2();
--- inst_experiments/out.js
+++
@@ -1,9 +0,0 @@
-__INST(0, null);const asdf = __INST(13, null, 1234);
-__INST(20, null);function asdf2() {
-__INST(37, 122);
-__INST(43, null);  __INST(43, null, __INST(43, null, __INST(43, null, console).log)(__INST(55, null, "asdf")));
-__INST(73, null);  let b = __INST(81, null, 3);
-__INST(88, null);  let c = __INST(96, null, 4);
-__INST(103, null);  let a = __INST(111, null, __INST(111, null, b) + __INST(115, null, 1) + __INST(119, null, c));
-}
-__INST(125, null);__INST(125, null, __INST(125, null, asdf2)());
